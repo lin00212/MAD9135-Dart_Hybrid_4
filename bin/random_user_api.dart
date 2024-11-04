8138a5fc46b4bf439b100eded2b815bc41de5231
Eason Lin <lin00212@algonquinlive.com>
2024-11-03 21:25:02 -0500

    Compeleted

---
+++ bin/random_user_api.dart
@@ -0,0 +1,21 @@
+import "dart:convert";
+import "package:http/http.dart" as http;
+
+void main() async {
+  const url = "https://random-data-api.com/api/users/random_user?size=10";
+
+  try {
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      List users = jsonDecode(response.body);
+      for (var user in users) {
+        print("${user["uid"]}: ${user["first_name"]} ${user["last_name"]}");
+      }
+    } else {
+      print("Failed to load user data: ${response.statusCode}");
+    }
+  } catch (e) {
+    print("An error occurred: $e");
+  }
+}
